6defe6928cbdf1bcb65d82dc2d858a07bc0952e4
minerest <eric.diaz13@yahoo.com>
2018-11-09 21:05:08 -0800

    added p@ss functionality w/ server/client reply system

--- client.py
+++ client.py
@@ -1,13 +1,41 @@
 import socket
+import ctypes
+import curses
+from time import sleep
 
-HOST = ''    # The remote host
-PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print("Connection complete")
-while True:
-    st = input("Enter str")
-    enc = st.encode()
-    s.sendall(enc)
-    data = s.recv(1024)
-s.close()
+
+def main():
+    try:
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+    except Exception as e:
+        print(e)
+
+
+    HOST = ''    # The remote host
+    PORT = 50004              # The same port as used by the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((HOST, PORT))
+        print("Connection complete")
+    except Exception as e:
+        print("Error with socket, Error output: ")
+        print(e)
+        return
+    while True:
+        st = input("Enter str")
+        enc = st.encode()
+        s.sendall(enc)
+        sleep(3)
+        for i in range(100):
+            data = s.recv(1024)
+            if data:
+                break
+        if data:
+            data = data.decode()
+            print(data)
+        else:
+            s.close()
+            return
+main()
--- server.py
+++ server.py
@@ -1,14 +1,35 @@
 import socket
+def main():
+    HOST = ''                 # Symbolic name meaning all available interfaces
+    PORT = 50004             # Arbitrary non-privileged port
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        print("Scoket bount to port", PORT)
+    except Exception as e:
+        print("ERROR WITH SOCKET. Error output: ")
+        print(e)
+        return
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024)
-    print(data)
-    conn.sendall(data)
-conn.close()
+        try:
+            print(data)
+            toSend = data.decode()
+        except:
+            pass
+        if 'AAA' in toSend:
+            toSend += " was recieved!!!"
+            print(toSend)
+            reply = "Thanks, your message was recieved!"
+            soSend2 = reply.encode()
+            conn.send(soSend2)
+        else:
+            print("WRONG FLAGs")
+            conn.close()
+            return
+
+main()
