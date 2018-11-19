5f106661b2b6cf8a968f5ff8caaad17ce63c7c1a
minerest <eric.diaz13@yahoo.com>
2018-11-19 14:47:42 -0800

    Added client system class and got cmd input on client.py

--- client.py
+++ client.py
@@ -1,55 +1,93 @@
+#!/usr/bin/python
+import sys #for command line args
 import socket
-import ctypes
-import curses
-from time import sleep
+#import ctypes
+# from random import choices as randCho
+# from string import ascii_uppercase
+# from string import digits
 
+
+class system:
+
+    def __init__(self):
+        self.host, self.port = self.configureConn()
+
+    def configureConn(self):
+        try:
+            int(sys.argv[2])
+            HOST, PORT = self.getCmd(sys.argv[1], sys.argv[2])
+        except Exception as e:
+            print(e)
+            HOST, PORT = self.getConfigFile()
+        return (HOST, int(PORT))
+
+    def getConfigFile(self):
+        # Gets the data within the ./config file.
+        # The return is a tuple containing the HOST and PORT settings.
+        with open("./config", 'r') as conf:
+            data = conf.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip('\n')
+        print(data)
+
+        # This is an object called configs. config.ky = val
+        # Step 1:get the length of the config file
+        # Step 2: split each line in the data file at the ':'
+        # step # step 3: set the split to a pair of variables ky and val.
+
+        configs = {ky: val for ky, val in (data[x].split(':') for x in range(len(data)))}
+        return (configs["HOST"], configs["PORT"])
+
+    def getCmd(self):
+        try:
+            PORT = int(PORT)
+        except:
+            print("Invalid port, pick one greater than 10,000")
+            sys.exit(1)
+        if isPortCorrect(PORT):
+            return (HOST, PORT)
+        else:
+            printHelp()
+            sys.exit(1)
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect((self.host, self.port))
+            print("Connection complete")
+        except Exception as e:
+            print(e)
+            sys.exit(1)
 
 def main():
-    try:
-        stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-    except Exception as e:
-        print(e)
+    # make the system class
+    Syst = system()
 
-    HOST, PORT = getConfig()
-    PORT = int(PORT)
-    # HOST = ''    # The remote host
-    # PORT = 50004              # The same port as used by the server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((HOST, PORT))
-        print("Connection complete")
-    except Exception as e:
-        print("Error with socket, Error output: ")
-        print(e)
-        return
+    # get IP and PORT from command line or conf file
+    Syst.configureConn()
+
+    # start the socket connection
+    Syst.connect()
     while True:
         st = input("Enter str")
+        print(st)
         enc = st.encode()
-        s.sendall(enc)
-        sleep(3)
-        for i in range(100):
-            data = s.recv(1024)
-            if data:
-                break
+        Syst.sock.sendall(enc)
+        data = Syst.sock.recv(1024)
         if data:
             data = data.decode()
             print(data)
         else:
-            s.close()
+            Syst.sock.close()
             return
 
-def getConfig():
-   #Gets the data within the ./config file.
-   #The return is a tuple containing the HOST and PORT settings.
-   with open("./config", 'r') as conf:
-       data = conf.readlines()
-   for i in range(len(data)):
-       data[i] = data[i].strip('\n')
-   print(data)
-   configs = {ky: val for ky, val in (data[x].split(':') for x in range(len(data)))}
 
-   return (configs["HOST"], configs["PORT"])
+def isPortCorrect(PORT):
+    if PORT < 10000 or PORT > 65000:
+        return False
+    return True
+
+def printHelp():
+    # Function that prints what the program expects from the command line
+    print("Example: python3 127.0.0.1 10000")
 
 main()
--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((HOST, PORT))
-        print("Scoket bount to port", PORT)
+        print("Socket bound to port", PORT)
     except Exception as e:
         print("ERROR WITH SOCKET. Error output: ")
         print(e)
@@ -17,34 +17,53 @@ def main():
     s.listen(1)
     conn, addr = s.accept()
     print('Connected by', addr)
+    i = 0
     while True:
-        data = conn.recv(1024)
-
         try:
-            print(data)
-            dataDec = data.decode()
+            data = conn.recv(1024)
         except:
-            conn.close()
-            return
+            data = ''
+            print("EMPTY")
+        i+=1
+        print(data)
+        dataDec = data.decode()
         if 'AAA' in dataDec:
+            dataDec = dataDec.replace('AAA','')
             dataDec += " was recieved!!!"
             print(dataDec)
             reply = "Thanks, your message was recieved!"
             repEnc = reply.encode()
             conn.send(repEnc)
-        else:
-            print("WRONG FLAGs")
+        elif 'exit' in dataDec or i == 50:
+            print("Exit flag recieved")
             conn.close()
-            return
+            s.listen(1)
+            conn, addr = s.accept()
+            print('Connected by', addr)
+
+        else:
+            print("Bad flags")
+            reply = "Bad flags"
+            repEnc = reply.encode()
+            conn.send(repEnc)
+
 
 def getConfig():
    #Gets the data within the ./config file.
    #The return is a tuple containing the HOST and PORT settings.
+
+    #This gets the lines of a file 1 by 1.
+   #The file is formatted with a ':' for key:value pairs
     with open("./config", 'r') as conf:
         data = conf.readlines()
     for i in range(len(data)):
         data[i] = data[i].strip('\n')
     print(data)
+
+   #This is an object called configs. config.ky = val
+                                #        Step 1:get the length of the config file
+                        #Step 2: split each line in the data file at the ':'
+                #step 3: set the split to a pair of variables ky and val.
     configs = {ky:val for ky, val in (data[x].split(':') for x in range(len(data)))}
 
     return (configs["HOST"], configs["PORT"])
